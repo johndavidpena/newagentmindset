28447d28f9958cc0400ad87cc9bf183f1389e3e1
John Pena <pemu775@gmail.com>
2020-09-14 16:19:14 -0600

    Updated sitemap url

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -28,7 +28,7 @@ const prettier = require('prettier');
 
         return `
                         <url>
-                            <loc>${`https://penaelpaso.com${route}`}</loc>
+                            <loc>${`https://newagentmindset.com${route}`}</loc>
                         </url>
                     `;
       })
